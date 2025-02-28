990590d32cdcc6391509985d2d7cc63df4ee50aa
shisui06 <127260275+shisui06@users.noreply.github.com>
2025-02-28 13:50:22 -0500

    fixed lunch

---
+++ src/app/about/page.tsx
@@ -0,0 +1,4 @@
+// Page "À propos" (route "/about")
+export default function About() {
+  return <div>Page À propos</div>;
+} 
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Home as HomeIcon, User, Briefcase, FileText } from 'lucide-react'
+import { NavBar } from "@/components/ui/tubelight-navbar"
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <h1>Welcome to my world my beautiful creature</h1>
+    
+      </div>
+    </div>
+  );
+}
