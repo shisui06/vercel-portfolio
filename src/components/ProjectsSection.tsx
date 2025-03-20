e2cf5d1ba71e4a52ee70a413a49733aefaec7796
shisui06 <127260275+shisui06@users.noreply.github.com>
2025-03-20 10:59:20 -0400

    changed the shimmer button of the projects section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -4,6 +4,7 @@ import { projectsData } from "@/data/projects";
 import { ThreeDCard } from "@/components/ui/3d-card";
 import LogoCarousel from "@/components/custom/LogoCarousel";
 import ProjectTag from "@/components/ProjectTag";
+import { ShimmerButton } from "@/components/ui/shimmer-button";
 
 
 export function ProjectsSection() {
@@ -28,21 +29,24 @@ export function ProjectsSection() {
           Mes projets
         </h1>
         <div className="px-4 flex gap-2 py-6 text-white sm:justify-center bg-black/80 backdrop-blur-lg rounded-lg border border-white/10 shadow-xl mb-8 z-10 relative">
-          <ProjectTag
+          <ShimmerButton 
             onClick={() => handleFilter("All")}
-            name="All"
-            isSelected={filter === "All"}
-          />
-          <ProjectTag
+            className="mt-4"
+          >
+            All
+          </ShimmerButton>
+          <ShimmerButton 
             onClick={() => handleFilter("Web")}
-            name="Web"
-            isSelected={filter === "Web"}
-          />
-          <ProjectTag
+            className="mt-4 ml-4"
+          >
+            Web
+          </ShimmerButton>
+          <ShimmerButton 
             onClick={() => handleFilter("Mobile")}
-            name="Mobile"
-            isSelected={filter === "Mobile"}
-          />
+            className="mt-4 ml-4"
+          >
+            Mobile
+          </ShimmerButton>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {filteredProjects.map((project) => (
